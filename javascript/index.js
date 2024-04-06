ada8c2aab60f9abfa0c74c89953e24005a9d053e
IlariaViglino <ilaria.viglino@yahoo.it>
2024-04-06 12:28:58 +0200

    multiple locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,46 @@
-let londonElement=document.querySelector("#london");
-let sydneyElement=document.querySelector("#sydney");
+//let londonElement=document.querySelector("#london");
+//let sydneyElement=document.querySelector("#sydney");
 
-function cityTimeZone(city){
-    if(city===london){
-        return "Europe/London";
+
+
+
+//let londonTime=londonElement.querySelector(".time");
+//function displayCityTime(city){
+  //  let cityTime=city.querySelector(".time");
+    //cityTime.innerHTML=moment().tz(cityTimeZone(city)).format("H:mm:ss [<small>]A[</small>]");
+//}
+
+
+//function cityTime(city){
+  //  let cityDate=city.querySelector(".date");
+    //let currentCityDate=moment().format("MMMM Do, YYYY");
+    // cityDate.innerHTML=currentCityDate;
+     //let cityTime=city.querySelector(".time");
+//setInterval(displayCityTime,1000,city);
+//}
+//cityTime(londonElement);
+//cityTime(sydneyElement);
+
+
+
+
+function updateCity(event){
+    if(event.target.value.length>0){
+        let cityTimeZone=event.target.value;
+        let cityName=cityTimeZone.replace("_"," ").split("/")[1];
+        let cityTime=moment().tz(cityTimeZone);
+        let citiesElement=document.querySelector("#cities");
+        citiesElement.innerHTML=`<div class="city">
+        <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("H:mm:ss [<small>]A[</small>]")}</div>
+    </div>
+    `
     }
-    if(city===sydney){
-        return "Australia/Sydney";
-    }
 }
 
 
-let londonTime=londonElement.querySelector(".time");
-function displayCityTime(city){
-    let cityTime=city.querySelector(".time");
-    cityTime.innerHTML=moment().tz(cityTimeZone(city)).format("H:mm:ss [<small>]A[</small>]");
-}
-
-
-function cityTime(city){
-    let cityDate=city.querySelector(".date");
-    let currentCityDate=moment().format("MMMM Do, YYYY");
-     cityDate.innerHTML=currentCityDate;
-     let cityTime=city.querySelector(".time");
-setInterval(displayCityTime,1000,city);
-}
-cityTime(londonElement);
-cityTime(sydneyElement);
+let citiesSelect=document.querySelector("#select-cities");
+citiesSelect.addEventListener("change",updateCity);
