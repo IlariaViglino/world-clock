2ff15aff0e8cceb1a0960bf9820e7604012cda68
IlariaViglino <ilaria.viglino@yahoo.it>
2024-04-07 21:16:39 +0200

    added locations and link

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,33 @@
-//let londonElement=document.querySelector("#london");
-//let sydneyElement=document.querySelector("#sydney");
+let londonElement=document.querySelector("#london");
+let losAngelesElement=document.querySelector("#losAngeles");
+
+let londonTime=londonElement.querySelector(".time");
+let losAngelesTime=losAngelesElement.querySelector(".time");
+
+function displayCityTime(city){
+    let cityTime=city.querySelector(".time");
+    let cityTimeZone;
+    if(city===londonElement){
+        cityTimeZone="Europe/London";  
+     }
+     if(city===losAngelesElement){
+         cityTimeZone="America/Los_Angeles";
+     }
+    cityTime.innerHTML=moment().tz(cityTimeZone).format("H:mm:ss [<small>]A[</small>]");
+}
 
 
 
-
-//let londonTime=londonElement.querySelector(".time");
-//function displayCityTime(city){
-  //  let cityTime=city.querySelector(".time");
-    //cityTime.innerHTML=moment().tz(cityTimeZone(city)).format("H:mm:ss [<small>]A[</small>]");
-//}
-
-
-//function cityTime(city){
-  //  let cityDate=city.querySelector(".date");
-    //let currentCityDate=moment().format("MMMM Do, YYYY");
-    // cityDate.innerHTML=currentCityDate;
-     //let cityTime=city.querySelector(".time");
-//setInterval(displayCityTime,1000,city);
-//}
-//cityTime(londonElement);
-//cityTime(sydneyElement);
+function cityTime(city){
+   let cityDate=city.querySelector(".date");
+    let currentCityDate=moment().format("MMMM Do, YYYY");
+     cityDate.innerHTML=currentCityDate;
+     let cityTime=city.querySelector(".time");
+     displayCityTime(city);
+setInterval(displayCityTime,1000,city);
+}
+cityTime(londonElement);
+cityTime(losAngelesElement);
 
 
 
@@ -31,19 +39,24 @@ function updateCity(event){
             cityTimeZone=moment.tz.guess();
         }
         let cityName=cityTimeZone.replace("_"," ").split("/")[1];
-        let cityTime=moment().tz(cityTimeZone);
-        let citiesElement=document.querySelector("#cities");
-        citiesElement.innerHTML=`<div class="city">
+    let cityTime=moment().tz(cityTimeZone);
+    let citiesElement=document.querySelector("#cities");
+            citiesElement.innerHTML=`<div class="city">
+            <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("H:mm:ss [<small>]A[</small>]")}</div>
+        </div>
         <div>
-    <h2>${cityName}</h2>
-    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("H:mm:ss [<small>]A[</small>]")}</div>
-    </div>
-    `
+        <a href="https://world-clockish.netlify.app/">All cities</a>
+        </div>
+        `;
+
     }
 }
 
-
 let citiesSelect=document.querySelector("#select-cities");
-citiesSelect.addEventListener("change",updateCity);
+citiesSelect.addEventListener("change", updateCity);
+
+
